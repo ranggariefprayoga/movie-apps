c6fa41124a5e820a67ed426793e04eb8b8a2fca5
Rangga Arief Prayoga <ranggaarief04032002@gamil.com>
2023-09-05 16:20:49 +0700

    all image done!

--- src/component/DetailMenu.jsx
+++ src/component/DetailMenu.jsx
@@ -16,7 +16,7 @@ function DetailMenu({ foods, drinks }) {
         <SwiperSlide className=" bg-white foods grid grid-rows-[0.5fr_2fr] sm:grid-rows-1 sm:grid-cols-[0.8fr_2fr] w-full shadow-xl rounded-lg">
           {" "}
           <div className="bg-gradient-to-r from-fuchsia-500 info flex justify-center items-center sm:flex-col p-2">
-            <img src="../../public/restaurants.svg" alt={foods.name} loading="lazy" className="w-6 h-6" />
+            <img src="/restaurants.svg" alt={foods.name} loading="lazy" className="w-6 h-6" />
             <h2 className="font-menu font-bold self-center text-xl">Our Foods!</h2>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-[1fr_1fr] ">
@@ -28,7 +28,7 @@ function DetailMenu({ foods, drinks }) {
         <SwiperSlide className="bg-white drinks grid grid-rows-[0.5fr_2fr] sm:grid-rows-1 sm:grid-cols-[0.8fr_2fr] w-full shadow-xl rounded-lg">
           {" "}
           <div className="bg-gradient-to-r from-fuchsia-500 info flex justify-center items-center sm:flex-col p-2">
-            <img src="../../public/restaurants.svg" alt={foods.name} loading="lazy" className="w-6 h-6" />
+            <img src="/restaurants.svg" alt={foods.name} loading="lazy" className="w-6 h-6" />
             <h2 className="font-menu font-bold self-center text-xl">Our Drinks!</h2>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-[1fr_1fr]  ">
