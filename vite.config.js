74685b0f4af0bb8ee83e676cb4f49eba22d07010
Rangga Arief Prayoga <ranggaarief04032002@gamil.com>
2023-09-05 11:03:04 +0700

    add new configuration

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split(".")[1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-      },
-    },
+    outDir: "dist",
   },
 });
